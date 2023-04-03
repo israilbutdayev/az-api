4ccd57d70e9b84202f5c5413517dae07b78cb90f
Israil Butdayev <israilbutdayev@gmail.com>
2023-04-03 01:03:07 +0000

    Update app.js and app.ts

--- dist/app.js
+++ dist/app.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import router from './routers/list.js';
 dotenv.config();
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 app.use('/list', router);
 app.get('/', (req, res) => {
     res.send('Express + TypeScript Server');
