e316f4b2e08977ba60e4b3991f027a367b3acbea
Israil Butdayev <israilbutdayev@gmail.com>
2023-04-07 05:48:36 +0400

    cbar implementation

--- dist/routers/cbar/index.js
+++ dist/routers/cbar/index.js
@@ -1,18 +1,46 @@
 import axios from "axios";
 import jsdom from "jsdom";
+// axios.defaults.proxy = {
+//   protocol: "http",
+//   host: "localhost",
+//   port: 8866,
+// };
 const { JSDOM } = jsdom;
-const home_url = 'https://www.cbar.az';
-const resp = await (await axios.get(home_url)).data;
-const dom = new JSDOM(resp);
-const csrf_token = dom.window.document.querySelector(`head > meta[name="csrf-token"]`)?.content || '';
-const from_date = '01/01/2023';
-const to_date = '07/04/2023';
-const currency = 'usd';
-const body = `_csrf=${encodeURIComponent(csrf_token)}&CurrencyForm%5BdateFrom%5D=${encodeURIComponent(from_date)}&CurrencyForm%5BdateTo%5D=${encodeURIComponent(to_date)}&CurrencyForm%5BcurrencyCode%5D=${currency}`;
-const response = await (await axios.post("https://cbar.az/currency/custom", body, {
+const home_url = "https://www.cbar.az";
+const request_home = await axios.get(home_url);
+const csrf_cookie = request_home.headers["set-cookie"]
+  .find((v) => v.includes("_csrf"))
+  .split(";")[0];
+const response_home = request_home.data;
+const dom = new JSDOM(response_home);
+const csrf_token =
+  dom.window.document.querySelector(`head > meta[name="csrf-token"]`)
+    ?.content || "";
+const from_date = "01/01/2023";
+const to_date = "07/04/2023";
+const currency = "usd";
+const body = `_csrf=${encodeURIComponent(
+  csrf_token
+)}&CurrencyForm%5BdateFrom%5D=${encodeURIComponent(
+  from_date
+)}&CurrencyForm%5BdateTo%5D=${encodeURIComponent(
+  to_date
+)}&CurrencyForm%5BcurrencyCode%5D=${currency}`;
+const response = await (
+  await axios.post("https://cbar.az/currency/custom", body, {
     headers: {
-        "content-type": "application/x-www-form-urlencoded",
+      "content-type": "application/x-www-form-urlencoded",
+      cookie: csrf_cookie,
     },
-})).data;
+  })
+).data;
 const currency_dom = new JSDOM(response);
-const table = currency_dom.window.document.querySelector("#mm-0 > div.content_wrap > div > div > div > div.table_wrap > div.table_content > div > div.table_items.table-pagination.paginate.paginate-0");
+const table = currency_dom.window.document.querySelector(
+  "body > div > div.content_wrap > div > div > div > div.table_wrap > div.table_content > div > div.table_items.table-pagination"
+);
+const output = [...table.children].map((tr) => {
+  const date = tr.querySelector(".valuta").textContent;
+  const rate = tr.querySelector(".kod").textContent;
+  return { currency, date, rate };
+});
+console.log(output);
